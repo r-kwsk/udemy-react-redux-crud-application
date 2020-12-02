0146ad8f5473f8b1e95206bc237edac46a523235
Rio Kawasaki <r.kwsk.0612@gmail.com>
2020-12-03 07:06:10 +0900

    Cloning teachers branch

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';//これ必要なの？
-
-function App() {
-  let message = 'hello world';
-  return <div>{message}</div>;
-}
-
-export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,4 @@
-//全reducerを一つにまとめる役割
 import { combineReducers } from 'redux'
 import count from './count'
 
 export default combineReducers({ count })
-// export default combineReducers({ foo, bar, baz });
