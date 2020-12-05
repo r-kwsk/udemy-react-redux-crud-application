5df9007cd6f920d0cfc59abc55fa5ffe2917a7ce
Rio Kawasaki <r.kwsk.0612@gmail.com>
2020-12-05 18:11:19 +0900

    implement delete feature

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,12 +1,15 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import { READ_EVENTS, DELETE_EVENT } from '../actions'
 
 //eventsはstateのこと．わかりやすく名前を変更
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.response.data, "id");
+    case DELETE_EVENT:
+      delete events[action.id]//eventにおけるactionから渡ってくるidを削除
+      return { ...events }//削除後のeventsを再レンダリング
     default:
-      return events
+      return events;
   }
 }
