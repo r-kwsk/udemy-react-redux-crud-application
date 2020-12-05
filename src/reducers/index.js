9f346f91330a9461340346d8e0ebb132cec9c5a7
Rio Kawasaki <r.kwsk.0612@gmail.com>
2020-12-05 14:46:29 +0900

    implement event push

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 //全reducerを結合するためのファイル
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
