dc64394b7186a7c91c9bcf25a45c4a0d865dbe2a
Rio kawasaki <52302993+r-kwsk@users.noreply.github.com>
2020-12-06 19:55:29 +0900

    Update count.js

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -5,7 +5,7 @@ const initialState = { value: 0 }
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return { value: state.value + 1 }
+      return { value: state.value + 1 }//stateを変更する場合はオブジェクトで記述
     case DECREMENT:
       return { value: state.value - 1 }
     default:
